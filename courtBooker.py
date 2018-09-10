bc03207788d39bfa8233dbab11fce95b4970d40f
Matthew1478 <matthew14macartney@hotmail.co.uk>
2018-09-10 23:16:12 +0100

    Added check to see if courts are bookable yet

--- courtBooker.py
+++ courtBooker.py
@@ -1,15 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 import sys
 # Import smtplib for the actual sending function
 import smtplib
-
-# Import the email modules we'll need
-from email.mime.text import MIMEText
 
 print(sys.argv[1])
 print(sys.argv[2])
@@ -69,40 +62,77 @@ class CourtBooker:
 
         time.sleep(5)
 
+        view_courts = self.driver.find_element_by_xpath("//input[@value=\"View Courts\"]")
+
         courtA = self.driver.find_element_by_xpath("//*[@id=\"CourtsGrid\"]/div[11]/div[9]")
         titleA = courtA.get_attribute("title")
-        print(titleA)
-
-        courtC = self.driver.find_element_by_xpath("//*[@id=\"CourtsGrid\"]/div[13]/div[9]")
-        titleC = courtC.get_attribute("title")
-        print(titleC)
-
-        court6 = self.driver.find_element_by_xpath("//*[@id=\"CourtsGrid\"]/div[8]/div[9]")
-        title6 = court6.get_attribute("title")
-        print(title6)
+        print("Court A: " + titleA)
 
         court5 = self.driver.find_element_by_xpath("//*[@id=\"CourtsGrid\"]/div[7]/div[9]")
         title5 = court5.get_attribute("title")
-        print(title5)
+        print("Court 5: " + title5)
+
+        while titleA == 'This slot falls outside of the booking window.' \
+                or title5 == 'This slot falls outside of the booking window.':
+            courtA = self.driver.find_element_by_xpath("//*[@id=\"CourtsGrid\"]/div[11]/div[9]")
+            titleA = courtA.get_attribute("title")
+            print("Court A: " + titleA)
+            court5 = self.driver.find_element_by_xpath("//*[@id=\"CourtsGrid\"]/div[7]/div[9]")
+            title5 = court5.get_attribute("title")
+            print("Court 5: " + title5)
+            if titleA != 'This slot falls outside of the booking window.' \
+                    and title5 != 'This slot falls outside of the booking window.':
+                break
+                print("Courts open")
+            else:
+                print('Trying again')
+                view_courts.click()
+                time.sleep(5)
+
+        courtC = self.driver.find_element_by_xpath("//*[@id=\"CourtsGrid\"]/div[13]/div[9]")
+        titleC = courtC.get_attribute("title")
+        print("Court C: " + titleC)
+
+        court6 = self.driver.find_element_by_xpath("//*[@id=\"CourtsGrid\"]/div[8]/div[9]")
+        title6 = court6.get_attribute("title")
+        print("Court 6: " + title6)
+
+        court5 = self.driver.find_element_by_xpath("//*[@id=\"CourtsGrid\"]/div[7]/div[9]")
+        title5 = court5.get_attribute("title")
+        print("Court 5: " + title5)
+
+        courtB = self.driver.find_element_by_xpath("//*[@id=\"CourtsGrid\"]/div[12]/div[9]")
+        titleB = courtB.get_attribute("title")
+        print("Court B: " + titleB)
 
         if 'Book this slot' in titleA:
             courtA.click()
-            time.sleep(5)
+            time.sleep(3)
             self.make_booking('court A')
         elif 'Book this slot' in titleC:
             courtC.click()
-            time.sleep(5)
+            time.sleep(3)
             self.make_booking('court C')
         elif 'Book this slot' in title6:
             court6.click()
-            time.sleep(5)
+            time.sleep(3)
             self.make_booking('court 6')
         elif 'Book this slot' in title5:
             court5.click()
-            time.sleep(5)
+            time.sleep(3)
             self.make_booking('court 5')
+        elif 'Book this slot' in titleB:
+            courtB.click()
+            time.sleep(3)
+            self.make_booking('court B')
         else:
-            self.send_email('No courts available to be')
+            print(titleA)
+            print(titleB)
+            print(titleC)
+            print(title5)
+            print(title6)
+            print('No courts available')
+            self.send_email('No courts available to be', self.email, self.epassword)
 
     def make_booking(self, string):
 
@@ -112,19 +142,21 @@ class CourtBooker:
             select_players = self.driver.find_element_by_xpath(
                 "//div[@id='PlayerDropDownDiv']//button[@class=\"ui-multiselect ui-multiselect-selectionsummary ui-corner-all\"]")
             select_players.click()
-            time.sleep(2)
+            time.sleep(1)
             brian = self.driver.find_element_by_xpath("//input[@title='Brian Macartney']")
             brian.click()
             campbell = self.driver.find_element_by_xpath("//input[@title='Campbell Dunn']")
             campbell.click()
             robert = self.driver.find_element_by_xpath("//input[@title='Robert Bell']")
             robert.click()
+            print("Booked!!!")
             self.send_email(string, self.email, self.epassword)
         else:
             print("Court already booked")
             self.send_email('Court already', self.email, self.epassword)
 
-    def send_email(self, string, email, epassword):
+    @staticmethod
+    def send_email(string, email, epassword):
         s = smtplib.SMTP('smtp.gmail.com', 587)
         s.starttls()
         s.login(email, epassword)
